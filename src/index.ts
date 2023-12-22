d010aab8df6cca4af065d08a2de02195aa10b83f
Eik Emil Bengtsson <git.noreply@e16n.dev>
2023-12-22 22:08:57 +0100

    chore: removed danish mappings

--- src/index.ts
+++ src/index.ts
@@ -9,12 +9,6 @@ import { chatApps } from './layers/chat';
 import { symbolsMap } from './layers/symbols';
 
 const ruleset = [
-    rule('danish mappings').manipulators([
-        map('grave_accent_and_tilde', 'right_shift').to('non_us_backslash', 'right_shift'),
-        map('grave_accent_and_tilde').to('non_us_backslash'),
-        map('non_us_backslash').to('grave_accent_and_tilde'),
-        map('non_us_backslash', 'right_shift').to('grave_accent_and_tilde', 'right_shift'),
-    ]),
     simlayer('i', 'symbols-mode').manipulators(symbolsMap),
     simlayer('w', 'main-app-mode').manipulators(mainApps),
     simlayer('z', 'chat-mode').manipulators(chatApps),
