66effaa3cb48dd4c24d23b4cc36b4eaa0840c5b0
Eik Emil Bengtsson <git.noreply@e16n.dev>
2023-12-23 12:18:34 +0100

    chore: updated key bindings

--- src/layers/main-apps.ts
+++ src/layers/main-apps.ts
@@ -2,18 +2,17 @@ import { to$, toApp } from 'karabiner.ts'
 
 // `open -a` sometimes gets confused by the non-standard path
 function toNonStandard(name: string) {
-    return to$(`open ~/Applications/${name}.app`)
+    return to$(`open /Applications/${name}.app`)
 }
 
 export const mainApps = {
   j: toApp('Arc'),
   k: toApp('WezTerm'),
-  l: toApp('LogSeq'),
-  t: toApp('Things3'), 
+  l: toApp('Inkdrop'),
+  t: toApp('Things3'),
   m: toApp('Spotify'),
   b: toApp('Beekeeper Studio'),
-  i: toApp('Insomnia'),
-  p: toNonStandard('1Password'), 
+  p: toNonStandard('1Password'),
   ',': toApp('System Settings'),
 }
 
