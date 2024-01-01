048ec2c110cb841bad1c0df678198869a87befad
Eik Emil Bengtsson <git.noreply@e16n.dev>
2024-01-01 22:06:57 +0100

    chore(arc): improved hotkeys for arc

--- src/apps/arc.ts
+++ src/apps/arc.ts
@@ -2,23 +2,30 @@ import { ToEvent, ifApp, map, rule, toKey } from "karabiner.ts"
 
 const appFilter = ifApp('^company.thebrowser.Browser$')
 
-const arcMaps = {
+const arc = {
   newTab: [toKey('t', 'left_command')],
-  sidebar: [toKey('s', 'left_command')],
   commander: [toKey('t', 'left_command')],
-}
-
-const goToSpace = (spaceNumber: number): ToEvent => {
-  return toKey(spaceNumber as any, 'left_control')
+  sidebar: [toKey('s', 'left_command')],
+  copyUrl: [toKey('c', 'left_command')],
+  developerTools: [toKey('i', ['left_option', 'left_command'])],
+  archiveTab: [toKey('w', 'left_command')],
+  clearTabs: [toKey('k', ['left_command', 'left_shift'])],
+  goToSpace: (spaceNumber: number): ToEvent => {
+    return toKey(spaceNumber as any, 'left_control')
+  }
 }
 
 const mappings = [
-  map('n', 'Hyper').to(arcMaps.newTab),
-  map('s', 'Hyper').to(arcMaps.sidebar),
-  map('k', 'Hyper').to(arcMaps.commander),
-  map('1', 'Hyper').to(goToSpace(1)),
-  map('2', 'Hyper').to(goToSpace(2)),
-  map('3', 'Hyper').to(goToSpace(3)),
+  map('n', 'Hyper').to(arc.newTab),
+  map('s', 'Hyper').to(arc.sidebar),
+  map('k', 'Hyper').to(arc.commander),
+  map('i', 'Hyper').to(arc.developerTools),
+  map('c', 'Hyper').to(arc.copyUrl),
+  map('w', 'Hyper').to(arc.archiveTab),
+  map('q', 'Hyper').to(arc.clearTabs),
+  map('1', 'Hyper').to(arc.goToSpace(1)),
+  map('2', 'Hyper').to(arc.goToSpace(2)),
+  map('3', 'Hyper').to(arc.goToSpace(3)),
 ]
 
 export const arcMappings = rule('terminal', appFilter).manipulators(mappings)
